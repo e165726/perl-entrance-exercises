29538ee4d673a25d962ea5ea38ddd81253ca819c
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2019-01-20 15:23:05 +0900

    simple_calcのリファクタリング

--- 4th/ex/simple_calc.pl
+++ 4th/ex/simple_calc.pl
@@ -3,35 +3,28 @@ use strict;
 use warnings;
 use utf8;
 
-sub add{
-  my ($left,$right) = @_;
-  return $left + $right;
-}
+sub add {
+  my ($left, $right) = @_;
+  return  $left + $right;
+};
 
 sub min {
-  my ($left,$right) = @_;
-  return $left - $right;
-}
+  my ($left, $right) = @_;
+  return  $left - $right;
+};
 
 sub mul {
-  my ($left,$right) = @_;
-  return $left * $right;
-}
+  my ($left, $right) = @_;
+  return  $left * $right;
+};
 
 sub div {
-  my ($left,$right) = @_;
-  return $left / $right;
-}
+  my ($left, $right) = @_;
+  return  $left / $right;
+};
 
-my $a = 10;
-my $b = 3;
+print add(9,3), " === " , 9+3 ,"\n";
+print min(9,3), " === ", 9-3, "\n";
+print mul(9,3), " === ", 9*3, "\n";
+print div(9,3), " === ", 9/3, "\n";
 
-my $add_result = add($a,$b);
-my $min_result = min($a,$b);
-my $mul_result = mul($a,$b);
-my $div_result = div($a,$b);
-
-print "$a + $b = $add_result\n";
-print "$a - $b = $min_result\n";
-print "$a * $b = $mul_result\n";
-print "$a / $b = $div_result\n";
