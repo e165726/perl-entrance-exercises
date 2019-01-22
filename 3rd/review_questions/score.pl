eac92b24dfa4ff8714728d917833a013a86550f0
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2019-01-22 15:11:12 +0900

    Refactoring score.pl

--- 3rd/review_questions/score.pl
+++ 3rd/review_questions/score.pl
@@ -1,16 +1,14 @@
-# 参考サイト http://orange-factory.com/dnf/cgi_outputjson.html
-
 #!/usr/bin/env perl
 use strict;
 use warnings;
 use utf8;
 use Data::Dumper;
+use feature 'say';
 
 # JSON風Dumper (http://orange-factory.com/dnf/cgi_outputjson.html)
-$Data::Dumper::Indent = 0;	# インデントなし
-$Data::Dumper::Useqq = 1; 	# ハッシュのキーはダブルクォートでくくる
-$Data::Dumper::Terse = 1; 	# evalするための、最初の「$VAR =」は不要
-$Data::Dumper::Pair = ":";	# ハッシュの区切り文字
+$Data::Dumper::Useqq = 1;       # ハッシュのキーはダブルクォートでくくる
+$Data::Dumper::Terse = 1;       # evalするための、最初の「$VAR =」は不要
+$Data::Dumper::Pair = ":";      # ハッシュの区切り文字
 
 my $alice = {
   name        => 'Alice',
@@ -40,113 +38,77 @@ my $carol = {
   binary      => 50,
 };
 my $dave = {
-    name        => 'Dave',
-    country     => 'Canada',
-    perl        => 60,
-    python      => 11,
-    ruby        => 1,
-    php         => 100,
-    binary      => 100,
+  name        => 'Dave',
+  country     => 'Canada',
+  perl        => 60,
+  python      => 11,
+  ruby        => 1,
+  php         => 100,
+  binary      => 100,
 };
 my $ellen = {
-    name        => 'Ellen',
-    country     => 'America',
-    perl        => 1,
-    python      => 15,
-    ruby        => 0.5,
-    php         => 60,
-    binary      => 0.01,
+  name        => 'Ellen',
+  country     => 'America',
+  perl        => 1,
+  python      => 15,
+  ruby        => 0.5,
+  php         => 60,
+  binary      => 0.01,
 };
 
-my $average = {
-  Alice     => 0,
-  Bob       => 0,
-  Carol     => 0,
-  Dave      => 0,
-  Ellen     => 0,
-}; 
-
+my $average = {};
+my $highscore = {};
 my @people = ($alice,$bob,$carol,$dave,$ellen);
-my @languages_sum;
-my @england; 
-my $england_ref = \@england;
-my @america;
-my $america_ref = \@america;
-my @canada;
-my $canada_ref = \@canada;
-my @countries_member = ($england_ref,$canada_ref,$america_ref);
+my @languages = qw(perl python ruby php binary);
 
-foreach my $score (@people) {
-  # それぞれの合計値を求める
-  push @languages_sum , $score->{perl} + $score->{python} + $score->{ruby} + $score->{php} + $score->{binary}; 
-  
-  # 5段階評価を求め，出力する 
-  print "$score->{name} of 5 grade\n";
-  foreach my $key (keys $score) {
-    if ($key eq 'perl' or $key eq 'ruby' or $key eq 'python' or $key eq 'php' or $key eq 'binary'){
-      my $value = $score->{$key};
-      if ($value >= 0 and $value <= 19) {
-        print "$key :   \n" 
-      }elsif ($value >= 20 and $value <= 39) {
-        print "$key :  *\n" 
-      }elsif ($value >= 40 and $value <= 59) {
-        print "$key :  **\n" 
-      }elsif ($value >= 60 and $value <= 79) {
-        print "$key :  ***\n" 
-      }elsif ($value >= 80 and $value <= 99) {
-        print "$key :  ****\n" 
-      }else{
-        print "$key :  *****\n" 
-      }
-      
-      # 国ごとの,perl のスコアが 60 以上の人の名前を格納
-      if ($key eq 'perl' and $value >= 60) {
-        if ($score->{country} eq 'England') {
-          push @{$england_ref} , $score->{name}; 
-        }elsif ($score->{country}  eq 'Canada') {
-          push @{$canada_ref} , $score->{name}; 
-        }elsif ($score->{country} eq 'America') {
-          push @{$america_ref} , $score->{name}; 
-        }
-      }
-    }
+# 合計値取得
+for my $human (@people) {
+  for my $lang(@languages) {
+    $human->{sum} += $human->{$lang};
   }
-  print "\n";
 }
 
-# 合計値をそれぞれのhashに加える
-${$alice}{sum} = $languages_sum[0];
-${$bob}{sum}   = $languages_sum[1];
-${$carol}{sum} = $languages_sum[2];
-${$dave}{sum}  = $languages_sum[3];
-${$ellen}{sum} = $languages_sum[4];
+# 言語ごとの平均
+for my $human (@people) {
+  my $sum = 0;
+  for my $lang(qw(perl python ruby)) {
+    $sum += $human->{$lang}; 
+    my $num = () = qw(perl python ruby);
+    $average->{$human->{name}} = $sum / $num;
+  }
+}
 
-# それぞれの平均点を求め，$averageに加える
-${$average}{Alice} = ($alice->{perl} + $alice->{ruby} + $alice->{python}) / 5;
-${$average}{Bob}   = ($bob->{perl} + $bob->{ruby} + $bob->{python}) / 5;
-${$average}{Carol} = ($carol->{perl} + $carol->{ruby} + $carol->{python}) / 5;
-${$average}{Dave}  = ($dave->{perl} + $dave->{ruby} + $dave->{python}) / 5;
-${$average}{Ellen} = ($ellen->{perl} + $ellen->{ruby} + $ellen->{python}) / 5;
+# 国ごとのPerlハイスコアリスト
+for my $country (qw(England America Canada)) {
+  for my $human (@people) {
+    if($human->{perl} >= 60 and $human->{country} eq $country){
+      push @{$highscore->{$country}}, $human->{name};
+    }
+  }
+}
 
-#  国ごとのPerlハイスコアリストを作る
-my $highscore = {
-  England   => $england_ref,
-  Canada    => $canada_ref,
-  America   => $america_ref,
-};
-
-print "sum\n";
-print Dumper($alice);
-print Dumper($bob);
-print Dumper($carol);
-print Dumper($dave);
-print Dumper($ellen);
-print "\n";
-
-print "average\n";
+# 出力
+print Dumper(@people);
 print Dumper($average);
-print "\n";
 
-print "PerlHighScoreList\n";
+## 5段階評価の出力
+for my $human (@people) {
+  say $human->{name}; 
+  for my $lang(@languages) {
+    if ($human->{$lang} <= 19) {
+      say "   $lang  :       ";
+    }elsif($human->{$lang} <= 39){
+      say "   $lang  :  *    ";
+    }elsif($human->{$lang} <= 59){
+      say "   $lang  :  **   ";
+    }elsif($human->{$lang} <= 79){
+      say "   $lang  :  ***  ";
+    }elsif($human->{$lang} <= 99){
+      say "   $lang  :  **** ";
+    }else{
+      say "   $lang  :  *****";
+    }
+  }
+}
+
 print Dumper($highscore);
-print "\n";
--- 4th/review_questions/food.pl
+++ 4th/review_questions/food.pl
@@ -3,10 +3,6 @@ use strict;
 use warnings;
 use utf8;
 
-# $data に人物名と好きな食べ物を (スペース):(スペース) 区切りで与えています.
-# この変数から食べ物が何回出現したかカウントして表示させてください.
-
-
 my $data = q{
   papix : sushi
   moznion : soba
